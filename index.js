de77148c0a25cd4edfa9e29c23e49e5dde976ef0
SunilNeupane77 <sunilneupane957@gmail.com>
2024-08-08 15:18:44 +0545

    login logout completed

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
 const app=express();
 dotenv.config({});
 
+
+// middleware 
+app.use(express.json())
+
+// routes
+app.use("/api/v1/user",userRoute)
 
 
 app.listen(process.env.PORT ||4000 ,()=>{
