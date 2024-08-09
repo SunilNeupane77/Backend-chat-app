5d44a10b1efc8463f213ecb0f77c5d0bb7782c7b
SunilNeupane77 <sunilneupane957@gmail.com>
2024-08-09 08:53:20 +0545

    message route conpleted

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/database.js";
+import messageRoute from "./routes/messageRoute.js";
 import userRoute from "./routes/userRoute.js";
 const app = express();
 dotenv.config({});
 
 // middleware
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.use("/api/v1/user", userRoute);
-app.use(cookieParser());
+app.use("/api/v1/message",messageRoute);
+
 
 app.listen(process.env.PORT || 4000, () => {
   connectDB();
-  console.log(`server is running $process.env.PORT`);
+  console.log(`server is running ${process.env.PORT}`);
 });
